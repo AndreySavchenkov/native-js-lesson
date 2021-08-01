2c29046c82dc26d39642daf5133035037cd993e1
sandvik16 <sannndvik16@gmail.com>
2021-08-01 20:36:08 +0300

    added map, filter, find

---
+++ 44-1/01-08-2021.js
@@ -0,0 +1,77 @@
+let students = [
+    {
+        name: "Bob",
+        age: 22,
+        isMarried: true,
+        scores: 85
+    },
+    {
+        name: "Alex",
+        age: 21,
+        isMarried: true,
+        scores: 89
+    },
+    {
+        name: "Nick",
+        age: 20,
+        isMarried: false,
+        scores: 120
+    },{
+        name: "John",
+        age: 19,
+        isMarried: false,
+        scores: 100
+    }
+];
+
+
+
+//
+
+const getName = el => el.name
+
+console.log(students.map(getName));
+
+
+//пишем свой самопальный map--------------------------------------------------------------------------
+
+const map = (array, callback) => {
+    const newArray = [];
+    for(let i = 0; i < array.length; i++){
+        newArray[i] = callback(array[i])
+    }
+    return newArray;
+}
+
+console.log(map(students, getName))
+console.log(map(students, st => ({
+    ...st,
+    technologies: []
+})))
+
+//самопальный filter------------------------------------------------------------------------------------
+
+const filter = (array, callback) => {
+    const newArray = [];
+    for(let i = 0; i < array.length; i++){
+        if (callback(array[i])) {
+            newArray.push(array[i])
+        }
+    }
+
+    return newArray;
+}
+
+console.log(filter(students, st => !st.isMarried))
+
+//самопальный find----------------------------------------------------------------------------------------
+
+const find = (array, callback) => {
+    for(let i = 0; i < array.length; i++){
+        if (callback(array[i])) {
+            return array[i]
+        }
+    }
+}
+
+console.log(find(students, st => st.name === 'Bob'))
