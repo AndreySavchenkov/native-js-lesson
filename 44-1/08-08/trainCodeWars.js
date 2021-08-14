c25460de66fd5c5968a1d19731bed45a6aa543ad
sandvik16 <sannndvik16@gmail.com>
2021-08-14 13:06:57 +0300

    practice Codewars

---
+++ 44-1/08-08/trainCodeWars.js
@@ -0,0 +1,34 @@
+//
+// const str = "hello world!";
+//
+// const reverseStr = (str) => {
+//     let newArr = str.split(' ').reverse().join(' ')
+//     console.log(newArr);
+// }
+//
+// reverseStr(str);
+
+//---------------------------------------------------------------
+
+// let arr = [1,-2,0,4,5]
+//
+// let newArr=[]
+//
+// const reverseNumber = (arr) => {
+//     newArr = arr.map(number => {
+//         if(number < 0){
+//             return Math.abs(number)
+//         } if (number == 0) {
+//             return 0
+//        } else {
+//             return -number
+//        }
+//     })
+// }
+//
+// reverseNumber(arr)
+//
+// console.log(newArr)
+
+//-------------------------------------------------------------------
+
